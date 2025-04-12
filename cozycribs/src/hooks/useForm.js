3883143ffe8397d4bcb41b4a43554c8cd589a880
Aryavart Chandel <aryavartchandel2@gmail.com>
2025-04-13 00:18:54 +0530

    changed hook

--- cozycribs/src/hooks/useForm.js
+++ cozycribs/src/hooks/useForm.js
@@ -2,15 +2,6 @@ import { useState } from 'react';
 
 const useForm = (initialValues) => {
   const [values, setValues] = useState(initialValues);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  };
-
   return [values, handleChange];
 };
 
